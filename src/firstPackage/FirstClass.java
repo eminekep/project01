1857e408488f1e2925516697ffe03f55717e2d3c
eminekep <73520616+eminekep@users.noreply.github.com>
2020-11-08 18:18:43 +0100

    last change

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -14,6 +14,7 @@ public class FirstClass {
 		System.out.println("conflict from remote master branch ");
 		
 		System.out.println("conflict from remote master branch ");
+		System.out.println("watch out remote master branch ");
 		
 		
 		
